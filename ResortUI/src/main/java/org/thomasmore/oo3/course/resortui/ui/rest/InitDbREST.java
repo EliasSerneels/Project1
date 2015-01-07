5200511d2fcb86127a2a7ef48c00877ece74eca4
EliasSerneels <r0381171@student.thomasmore.be>
2015-01-07 02:14:59 +0100

    Verdere cleanup + initdb extra info

--- ResortUI/src/main/java/org/thomasmore/oo3/course/resortui/ui/rest/InitDbREST.java
+++ ResortUI/src/main/java/org/thomasmore/oo3/course/resortui/ui/rest/InitDbREST.java
@@ -18,6 +18,7 @@ package org.thomasmore.oo3.course.resortui.ui.rest;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.UUID;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -46,27 +47,27 @@ public class InitDbREST {
     public BungalowEntity createDB() {
         List<Object> objectsToSave = new LinkedList<>();
         BungalowEntity bungalowEntity = new BungalowEntity();
-        bungalowEntity.setName("ABC");
-        bungalowEntity.setType("Type 5");
-        bungalowEntity.setPrice(100);
+        bungalowEntity.setName("Bananendoos");
+        bungalowEntity.setType("Type 3");
+        bungalowEntity.setPrice(500000);
         bungalowEntity.setPark("Parkje 2");
-        bungalowEntity.setMaxpeople("5");
+        bungalowEntity.setMaxpeople("1");
         objectsToSave.add(bungalowEntity);
         
         BungalowEntity bungalowEntity2 = new BungalowEntity();
-        bungalowEntity2.setName("DEF");
-        bungalowEntity2.setType("Type 1");
-        bungalowEntity2.setPrice(150);
+        bungalowEntity2.setName("Tomattenbak");
+        bungalowEntity2.setType("Type 2");
+        bungalowEntity2.setPrice(10);
         bungalowEntity2.setPark("Parkje 1");
         bungalowEntity2.setMaxpeople("8");
         objectsToSave.add(bungalowEntity2);
         
         BungalowEntity bungalowEntity3 = new BungalowEntity();
-        bungalowEntity3.setName("GHI");
-        bungalowEntity3.setType("Type 2");
+        bungalowEntity3.setName("Paalwoning");
+        bungalowEntity3.setType("Type 4");
         bungalowEntity3.setPrice(80);
         bungalowEntity3.setPark("Parkje 2");
-        bungalowEntity3.setMaxpeople("52");
+        bungalowEntity3.setMaxpeople("5");
         objectsToSave.add(bungalowEntity3);
         
         ParkEntity parkEntity = new ParkEntity();
@@ -82,33 +83,33 @@ public class InitDbREST {
         objectsToSave.add(parkEntity2);
         
         CustomerEntity ce1 = new CustomerEntity();
-        ce1.setFirstname("jos");
-        ce1.setLastname("janssens");
+        ce1.setFirstname("Jos");
+        ce1.setLastname("Janssens");
         ce1.setBirthdate("01/01/1980");
         ce1.setReceiveupdate(false);
         objectsToSave.add(ce1);
                
         CustomerEntity ce2 = new CustomerEntity();
-        ce2.setFirstname("piet");
-        ce2.setLastname("uyttebroeck");
+        ce2.setFirstname("Piet");
+        ce2.setLastname("Uyttebroeck");
         ce2.setBirthdate("01/01/1970");
         ce2.setReceiveupdate(false);
         objectsToSave.add(ce2);
         
         UserEntity ue1 = new UserEntity();
-        ue1.setId("sjreqzehqmzegh");
+        ue1.setId(UUID.randomUUID().toString());
         ue1.setUsername("Pieter");
         ue1.setPassword("Pieter");
         objectsToSave.add(ue1);
         
         UserEntity ue2 = new UserEntity();
-        ue2.setId("sjreqzegqzeg");
+        ue2.setId(UUID.randomUUID().toString());
         ue2.setUsername("Timothy");
         ue2.setPassword("Timothy");
         objectsToSave.add(ue2);
         
         UserEntity ue3 = new UserEntity();
-        ue3.setId("sjrqzehzqezrzhqmzegh");
+        ue3.setId(UUID.randomUUID().toString());
         ue3.setUsername("Elias");
         ue3.setPassword("Elias");
         objectsToSave.add(ue3);
