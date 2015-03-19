93f466c7483d82f705964261800be5a38a08f6d2
Jeroendw95 <jeroen.dw2@hotmail.com>
2015-03-19 15:39:16 +0100

    eventcompany kleine aanpassing

---
+++ ResortUI/src/main/java/org/thomasmore/oo3/course/resortui/model/LocationListDetailDto.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.thomasmore.oo3.course.resortui.model;
+
+/**
+ *
+ * @author Jeroen
+ */
+public class LocationListDetailDto {
+
+    private String id;
+    private String city;
+    private String street;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+    
+    
+}
