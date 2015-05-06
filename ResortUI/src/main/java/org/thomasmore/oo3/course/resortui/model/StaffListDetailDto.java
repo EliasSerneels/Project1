48b38eac6bc147f8ece3f7b94de8cd291636e390
Jeroendw95 <jeroen.dw2@hotmail.com>
2015-05-06 20:13:56 +0200

    staff dto (2e keer) event management

---
+++ ResortUI/src/main/java/org/thomasmore/oo3/course/resortui/model/StaffListDetailDto.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.thomasmore.oo3.course.resortui.model;
+
+/**
+ *
+ * @author Jeroen
+ */
+public class StaffListDetailDto {
+
+    private String id;
+    private String firstname;
+    private String lastname;
+    private String birthdate;
+    private String country;
+    private String city;
+    private String street;
+    private int housenumber;
+    private String phonenumber;
+    private String email;
+}
