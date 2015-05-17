ea8c8acf0b2797e8b8cd64b31a04965f326a319d
stijnzwaenepoel <r0368396@student.thomasmore.be>
2015-05-17 18:07:31 +0200

    T225: Enkel de bungalows tonen die in het geselecteerde park zitten, Bug opgelost dat Bungalows niet meer konden worden opgeslagen

--- ResortUI/src/main/java/org/thomasmore/oo3/course/resortui/controller/ReservationController.java
+++ ResortUI/src/main/java/org/thomasmore/oo3/course/resortui/controller/ReservationController.java
@@ -8,6 +8,7 @@ import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
 import javax.faces.application.FacesMessage;
+import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
 import org.primefaces.event.SelectEvent;
@@ -25,6 +26,7 @@ import org.thomasmore.oo3.course.resortui.model.ReservationPageDto;
 
 @Named(value = "reservation")
 @RequestScoped
+@ViewScoped
 public class ReservationController {
 
     private ReservationPageDto dto;
@@ -144,6 +146,15 @@ public class ReservationController {
 
     public void setSelectedReservation(List<ReservationEntity> selectedReservation) {
         this.selectedReservation = selectedReservation;
+    }
+
+    public void update(String parknaam) {
+        dto.getBungalowList().clear();
+        for (BungalowEntity bungalow : bungalows) {
+            if (bungalow.getPark().equals(parknaam)) {
+            dto.getBungalowList().add(bungalow.getName());
+            }
+        }
     }
 
 }
