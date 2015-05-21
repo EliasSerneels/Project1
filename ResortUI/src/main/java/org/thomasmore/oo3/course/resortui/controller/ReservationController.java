3ed57aaa8db24309732ad06e051b7e338df1dc74
t-mo-t <t-mo-t@live.be>
2015-05-21 02:07:43 +0200

    Dubbele boeking, CRUD en listen aan Reservation
    Slaapwel

--- ResortUI/src/main/java/org/thomasmore/oo3/course/resortui/controller/ReservationController.java
+++ ResortUI/src/main/java/org/thomasmore/oo3/course/resortui/controller/ReservationController.java
@@ -9,6 +9,7 @@ import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
 import javax.servlet.http.HttpServletRequest;
+import org.primefaces.context.RequestContext;
 import org.primefaces.event.SelectEvent;
 import org.thomasmore.oo3.course.resortui.business.entity.ReservationEntity;
 import org.thomasmore.oo3.course.resortui.facade.ReservationFacade;
@@ -37,8 +38,46 @@ public class ReservationController {
 
 
     public String add() {
-        reservationFacade.add(dto);
-        return pageRedirect;
+
+        String check = reservationFacade.checkBooking(dto);
+
+        // Check of dubbele boeking in string staat. Zo ja moet string opgespslits worden om suggestie te krijgen.
+        String suggestion = "";
+        if (check.contains("doubleBooking:")) {
+            suggestion = check.replace("doubleBooking: ", "");
+            check = "doubleBooking";
+        }
+
+        // Zak hoe dan ook naar het formulier wanneer één van deze meldingen getoond wordt
+        RequestContext context = RequestContext.getCurrentInstance();
+        context.scrollTo("frmEvent");
+
+        // check of er meldingen getoond moeten worden
+        switch (check) {
+            case "begindateAfterEnddate": {
+                FacesContext facesContext = FacesContext.getCurrentInstance();
+                FacesMessage facesMessage = new FacesMessage("De gekozen begindatum is later dan de gekozen einddatum.");
+                facesContext.addMessage(null, facesMessage);
+                return null;
+            }
+            case "doubleBooking": {
+                FacesContext facesContext = FacesContext.getCurrentInstance();
+                FacesMessage facesMessage = new FacesMessage("De gekozen locatie is reeds bezet voor deze datum. Gelieve een andere datum te kiezen.");
+                facesContext.addMessage(null, facesMessage);
+                // Roept de dialogbox op
+                FacesMessage message = new FacesMessage("Deze locatie is reeds volzet.", suggestion);
+                RequestContext.getCurrentInstance().showMessageInDialog(message);
+                return null;
+            }
+            case "beginDateBeforeToday": {
+                FacesContext facesContext = FacesContext.getCurrentInstance();
+                FacesMessage facesMessage = new FacesMessage("De datum die u gekozen hebt ligt in het verleden.");
+                facesContext.addMessage(null, facesMessage);
+                return null;
+            }
+            default:
+                return "reservation.xhtml?faces-redirect=true";
+        }
     }
 
     public void onDateSelect(SelectEvent event) {
--- ResortUI/src/main/java/org/thomasmore/oo3/course/resortui/facade/ReservationFacade.java
+++ ResortUI/src/main/java/org/thomasmore/oo3/course/resortui/facade/ReservationFacade.java
@@ -2,12 +2,17 @@ package org.thomasmore.oo3.course.resortui.facade;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
+import org.thomasmore.oo3.course.resortui.business.entity.BungalowEntity;
+import org.thomasmore.oo3.course.resortui.business.entity.CustomerEntity;
+import org.thomasmore.oo3.course.resortui.business.entity.ParkEntity;
 import org.thomasmore.oo3.course.resortui.business.entity.ReservationEntity;
 import org.thomasmore.oo3.course.resortui.controller.ScheduleController;
 import org.thomasmore.oo3.course.resortui.dao.BungalowDao;
@@ -33,6 +38,11 @@ public class ReservationFacade {
 
     private final SimpleDateFormat dateDate = new SimpleDateFormat("dd/MM/yyyy");
 
+    @PostConstruct
+    public void init() {
+        schedulecontroller.LoadBungalowSchedule();
+    }
+    
     public ReservationPageDto loadReservationOverviewPage(String editId, String deleteId) {
         ReservationPageDto dto = new ReservationPageDto();
         if (editId != null) {
@@ -50,19 +60,40 @@ public class ReservationFacade {
                 dto.getDetail().setStartDateFormatted(reservationEntity.getStartDateFormatted());
                 dto.getDetail().setStartTime(reservationEntity.getStartTime());
             }
+            schedulecontroller.LoadBungalowSchedule();
         }
-    if (deleteId != null) {
+        if (deleteId != null) {
             reservationsDao.deleteById(deleteId);
+            schedulecontroller.LoadBungalowSchedule();
         }
-   List<ReservationEntity> reservations = reservationsDao.listAll();
 
-    for(ReservationEntity reservation : reservations){
-        ReservationListDetailDto listDetail = new ReservationListDetailDto();
-        listDetail.setId(reservation.getId());
-        listDetail.setBungalowName(reservation.getBungalowName());
-        listDetail.setCustomerName(reservation.getCustomerName());
-        
-        // Datum formateren
+        List<CustomerEntity> customers = customersDao.listAll();
+
+        for (CustomerEntity customer : customers) {
+            dto.getCustomerList().add(customer.getFirstname() + " " + customer.getLastname());
+        }
+
+        List<ParkEntity> parks = parkDao.listAll();
+
+        for (ParkEntity park : parks) {
+            dto.getParkList().add(park.getName());
+        }
+
+        List<BungalowEntity> bungalows = bungalowsDao.listAll();
+
+        for (BungalowEntity bungalow : bungalows) {
+            dto.getBungalowList().add(bungalow.getName());
+        }
+
+        List<ReservationEntity> reservations = reservationsDao.listAll();
+
+        for (ReservationEntity reservation : reservations) {
+            ReservationListDetailDto listDetail = new ReservationListDetailDto();
+            listDetail.setId(reservation.getId());
+            listDetail.setBungalowName(reservation.getBungalowName());
+            listDetail.setCustomerName(reservation.getCustomerName());
+
+            // Datum formateren
             listDetail.setStartDateFormatted(schedulecontroller.DateAndTime(reservation.getStartDate(), reservation.getStartTime()));
             listDetail.setEndDateFormatted(schedulecontroller.DateAndTime(reservation.getEndDate(), reservation.getEndTime()));
 
@@ -76,16 +107,15 @@ public class ReservationFacade {
             listDetail.setStartDate(reservation.getStartTime());
             listDetail.setEndDate(reservation.getEndTime());
             // Tot hier wordt datum geformateerd
-            
-        
-        listDetail.setParkName(reservation.getParkName());
-        dto.getList().add(listDetail);
-        
-    }
-    return dto;
+
+            listDetail.setParkName(reservation.getParkName());
+            dto.getList().add(listDetail);
+
+        }
+        return dto;
     }
 
-    public ReservationPageDto add(ReservationPageDto dto){
+    public ReservationPageDto add(ReservationPageDto dto) {
         ReservationEntity reservationEntity = null;
         //als de id niet geset is kennen we hem 1 toe;
         if (dto.getDetail().getId() == null || dto.getDetail().getId().isEmpty()) {
@@ -94,7 +124,7 @@ public class ReservationFacade {
             reservationEntity = reservationsDao.findById(dto.getDetail().getId());
 
         }
-        if(reservationEntity == null){
+        if (reservationEntity == null) {
             reservationEntity = new ReservationEntity();
         }
         reservationEntity.setId(dto.getDetail().getId());
@@ -108,10 +138,58 @@ public class ReservationFacade {
         reservationEntity.setStartDateFormatted(dto.getDetail().getStartDateFormatted());
         reservationEntity.setStartTime(dto.getDetail().getStartTime());
         reservationsDao.save(reservationEntity);
-        
-        schedulecontroller.LoadEventSchedule();
-        
+
+        schedulecontroller.LoadBungalowSchedule();
+
         return dto;
     }
-    
+
+    public String checkBooking(ReservationPageDto dto) {
+
+        // Check of begintijd na eindtijd komt
+        if (dto.getDetail().getStartDate().after(dto.getDetail().getEndDate())) {
+            return "begindateAfterEnddate";
+        }
+
+        // Check of begintijd voor vandaag komt
+        if (dto.getDetail().getStartDate().before(schedulecontroller.yesterday())) {
+            return "beginDateBeforeToday";
+        }
+
+        // Dubbele boeking nagaan
+        List<ReservationEntity> reservations = reservationsDao.listAll();
+        for (ReservationEntity res : reservations) {
+
+            // Voeg datum en tijd samen
+            Date reservationStartDate = res.getStartDate();
+            Date reservationEndDate = res.getEndDate();
+            Date dtoStartDate = dto.getDetail().getStartDate();
+            Date dtoEndDate = dto.getDetail().getEndDate();
+
+            System.out.println(reservationStartDate + ", " + reservationEndDate + ", " + dtoStartDate + ", " + dtoEndDate);
+
+                // Checkt of datum en tijd tussen een reeds geboekte tijd ligt voor een bepaalde locatie
+            // OPGELET! De !=null moet aanwezig zijn want anders wordt een nullpointer gegeven wanneer vergeleken wordt met een lege waarde              
+            if (// Nullpointers vermijden
+                    reservationStartDate != null
+                    && reservationEndDate != null
+                    && res.getBungalowName() != null
+                    && // Check of gekozen datum niet dezelfde datum is dan begin of einddatum 
+                    (dtoStartDate.equals(reservationStartDate) && dto.getDetail().getBungalowName().equals(res.getBungalowName()))
+                    || (dtoEndDate.equals(reservationEndDate) && dto.getDetail().getBungalowName().equals(res.getBungalowName()))
+                    || (dtoStartDate.equals(reservationEndDate) && dto.getDetail().getBungalowName().equals(res.getBungalowName()))
+                    || (dtoEndDate.equals(reservationStartDate) && dto.getDetail().getBungalowName().equals(res.getBungalowName()))
+                    || // Als reservation tussen begin en einddatum ligt en Zelfde locatie heeft
+                    (dtoStartDate.before(reservationStartDate) && dtoEndDate.after(reservationEndDate) && dto.getDetail().getBungalowName().equals(res.getBungalowName()))
+                    || // Als begintijd = binnen range of eindtijd is binnen range     
+                    (dtoStartDate.after(reservationStartDate) && dtoStartDate.before(reservationEndDate) && dto.getDetail().getBungalowName().equals(res.getBungalowName()))
+                    || (dtoEndDate.after(reservationStartDate) && dtoEndDate.before(reservationEndDate) && dto.getDetail().getBungalowName().equals(res.getBungalowName()))) {
+                return "doubleBooking: <p>Locatie " + res.getBungalowName() + " is reeds volboekt van</p><p>" + res.getStartDateFormatted() + " tot " + res.getEndDateFormatted() + ".</p><p>Gelieve andere datum te selecteren.</p>";
+            }
+        }
+
+        add(dto);
+        return "";
+    }
+
 }
